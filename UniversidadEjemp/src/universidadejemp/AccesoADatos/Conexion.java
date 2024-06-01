2caa54dc4570fdb26afaae47b289a3cb0b9d86b8
JorgeFernandezRojoAndres <120122104+JorgeFernandezRojoAndres@users.noreply.github.com>
2024-05-31 23:17:40 -0300

    Gestión de Alumnos por Materia en Java Swing
    Componentes Principales:
    JComboBox: Aquí seleccionas la materia que quieres consultar.
    JTable: Una tabla que muestra los alumnos inscritos en la materia seleccionada con columnas para ID, DNI, apellido y nombre.
    JButton: Un botón de salida que cierra la ventana.
    Funcionamiento:
    Selección de Materia:
    
    Al iniciar la aplicación, el comboBox se llena con las materias disponibles.
    Al seleccionar una materia del comboBox, se dispara un evento que actualiza la tabla con los datos de los alumnos inscritos en esa materia.
    Carga de Datos:
    
    Se conecta a una base de datos MariaDB para obtener la lista de materias y alumnos.
    Utiliza el DefaultTableModel para gestionar los datos de la tabla.
    Conexión a la Base de Datos:
    
    Usa una clase Conexion para manejar la conexión a la base de datos MariaDB.
    La conexión se establece usando DriverManager y se maneja cualquier excepción que pueda surgir.

--- UniversidadEjemp/src/universidadejemp/AccesoADatos/Conexion.java
+++ UniversidadEjemp/src/universidadejemp/AccesoADatos/Conexion.java
@@ -1,11 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package universidadejemp.AccesoADatos;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.swing.JOptionPane;
 import org.mariadb.jdbc.Connection;
 import java.sql.DriverManager;
